1171fc3db1f7e89318799116df69916c571478d9
hgl868 <hgl868@126.com>
2016-05-17 10:27:30 +0800

    测试读取本地文件

--- cocos2d/cocos/base/CCConsole.cpp
+++ cocos2d/cocos/base/CCConsole.cpp
@@ -259,7 +259,7 @@ static void _log(const char *format, va_list args)
     strcat(buf, "\n");
 
 #if CC_TARGET_PLATFORM == CC_PLATFORM_ANDROID
-    __android_log_print(ANDROID_LOG_DEBUG, "cocos2d-x debug info", "%s", buf);
+    __android_log_print(ANDROID_LOG_DEBUG, "novo info", "%s", buf);
 
 #elif CC_TARGET_PLATFORM ==  CC_PLATFORM_WIN32 || CC_TARGET_PLATFORM == CC_PLATFORM_WINRT
 
